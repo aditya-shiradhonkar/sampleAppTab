2c4441845d0bef5a02956df9db2d48d83a4c5bad
Aniruddha Arvind Trikolker <aniruddhatr@cybage.com>
2016-12-27 15:09:58 +0530

    Removed unwanted imports

--- sampleAppTab/app/src/main/java/example/com/sampleapptab/login/model/IAuthenticationRequests.java
+++ sampleAppTab/app/src/main/java/example/com/sampleapptab/login/model/IAuthenticationRequests.java
@@ -4,19 +4,14 @@ package example.com.sampleapptab.login.model;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 /**
- * Created by aniruddhatr on 12/22/2016.
+ * Created by aniruddhatr on 12/22/2016. This interface would contain all the API's related to
+ * authentication
  */
 public interface IAuthenticationRequests {
 
-  /*  @Headers({
-            "Content-Type : application/x-www-form-urlencoded",
-            "Accept : application/json"
-    })
-  */
     @FormUrlEncoded
     @POST("auth/token.php")
     Call<AuthenticationResponseModel> sendAuthenticationDetails(
